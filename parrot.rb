854a29b72dae6fbf4f5db21e8c4a47267135d993
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-21 16:42:00 -0500

    commit tree 59f106209287ff034ae2606f3c51b291fb2a6b6f

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# def parrot(sentence = "Squawk!")
+#     puts sentence
+#     return sentence
+# end
+
+def parrot(defaultPhrase = "Squawk!")
+    puts defaultPhrase
+    return defaultPhrase
+end
+parrot("Pretty bird!")
